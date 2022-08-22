2e5d895f4b64dec05c3afc5c80837752bf86ae99
kauntak <n.koshirae+github@gmail.com>
2022-08-21 19:57:53 -0600

    Update HomeScreen.tsx
    Adding Home screen animations

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -18,16 +18,31 @@ export const HomeScreen:React.FC<Props> = ({scrollPosition})=>{
         width:"100%",
         display:"flex",
         flexDirection:"column",
-        justifyContent:"center",
         alignContent:"center",
         textAlign:"center"
     });
+    const [nameSentence, setNameSentence]  = useState<string>("");
+    const [nickNameSentence, setNickNameSentence]  = useState<string>("");
+    const [loveSentence, setLoveSentence]  = useState<string>("");
+    const [titleSentence, setTitleSentence] = useState<string>("");
     const [name, setName] = useState<string>("Nozomu Koshirae");
     const [nameStyle, setNameStyle] = useState<CSSProperties>({});
     const [title, setTitle] = useState<string>("");
     const [titleStyle, setTitleStyle] = useState<CSSProperties>({});
     const screenSize = useContext(ScreenSizeContext);
+    const loveList = [
+        "making things",
+        "problem solving",
+        "team work",
+        "self-reliance",
+        "learning",
+        "everything computer related!"
+    ];
 
+    const [listStyle, setListStyle] = useState<CSSProperties[]>(new Array(loveList.length + 1).fill({
+        transition:"all 0.5s ease-in-out",
+        opacity: 0
+    }));
     useEffect(()=> {
         if(isMinified) {
             const newValue = Math.floor((60 - (scrollPosition / 7)) * 10) / 10;
@@ -81,6 +96,7 @@ export const HomeScreen:React.FC<Props> = ({scrollPosition})=>{
         } else {
             setMainStyle({
                 padding: 30,
+                paddingLeft: "200px",
                 overflowY: "hidden",
                 height:"100%",
                 display:"flex",
@@ -89,7 +105,8 @@ export const HomeScreen:React.FC<Props> = ({scrollPosition})=>{
             setLeftStyle({
                 display:"flex",
                 flexDirection:"column",
-                minWidth:"30%"
+                minWidth:"30%",
+                paddingTop: "20vh"
             })
             setGlobeStyle({justifyContent:"center", display:"none"});
             setProfilePicStyle({
@@ -100,7 +117,7 @@ export const HomeScreen:React.FC<Props> = ({scrollPosition})=>{
                 display: "block",
                 marginLeft: "auto",
                 marginRight: "auto",
-                position:"absolute",
+                top: "1vh"
             });
             setName("");
             setTitle("");
@@ -108,29 +125,100 @@ export const HomeScreen:React.FC<Props> = ({scrollPosition})=>{
         }
     }, [isMinified]);
 
-    const animateText = (setter:Dispatch<SetStateAction<string>>, stringToEdit:string, stringTo:string) => {
+    const animateText:(obj:StringAnimationType)=>void = ({setter, stringTo, sideEffect}) => {
         return new Promise<void>(resolve => {
             const textInterval = setInterval(()=> {
-                let i:number;
-                for(i = 0; i < stringToEdit.length; i++){
-                    if(stringToEdit.charAt(i) !== stringTo.charAt(i)){
-                        setter(oldString => oldString.slice(0, -1));
-                        return;
+                setter(oldString => {
+                    let i: number;
+                    for(i = 0; i < oldString.length; i++){
+                        if(oldString.charAt(i) !== stringTo.charAt(i))
+                            return oldString.slice(0, -1);
                     }
+                    if(i < stringTo.length) {
+                        return oldString + stringTo.charAt(i);
+                    } else {
+                        clearInterval(textInterval);
+                        if(sideEffect) sideEffect();
+                        resolve();
+                        return oldString;
+                    }
+                });
+            }, 50)
+        });
+    }
+    type StringAnimationType = {
+        setter:Dispatch<SetStateAction<string>>, stringTo:string, sideEffect?:Function
+    }
+    const animateStringList = async (list:StringAnimationType[]) =>{
+        for(let i = 0; i < list.length; i++){
+            await animateText(list[i]);
+        }
+    }
+    const startAnimation = () => {
+        const list:StringAnimationType[] = [
+            {
+                setter:setNameSentence,
+                stringTo:"Hi! I'm Nozomu Koshirae."
+            },
+            {
+                setter:setName,
+                stringTo:"Nozomu Koshirae"
+            },{
+                setter:setNickNameSentence,
+                stringTo:"I go by Non."
+            },{
+                setter:setName,
+                stringTo:"Non"
+            },{
+                setter:setLoveSentence,
+                stringTo:"I Love...",
+                sideEffect: () => startListTransition(1)
+            }
+        ];
+        animateStringList(list);
+    }
+
+    const startListTransition = (opacity:number) => {
+        let i = 0;
+        const listInterval = setInterval(()=>{
+            if(i === listStyle.length){
+                clearInterval(listInterval);
+            }
+            setListStyle(oldStyle => {
+                const newStyle = [...oldStyle];
+                newStyle[i] = {
+                    transition:"all 0.5s ease-in-out",
+                    opacity
+                };
+                return newStyle;
+            });
+            i++;
+        }, 200)
+        
+    }
+
+    const onListTransitionEnd = (e:React.TransitionEvent<HTMLLIElement>) => {
+        if(e.currentTarget.style.opacity === "1"){
+            const list:StringAnimationType[] = [
+                {
+                    setter: setTitleSentence,
+                    stringTo:"I'm a Software Developer!"
+                }, {
+                    setter: setTitle,
+                    stringTo:"Software Developer",
+                    sideEffect: ()=> transitionGlobe
                 }
-                if(i < stringTo.length){
-                    setter(oldString => oldString + stringTo.charAt(i));
-                } else {
-                    clearInterval(textInterval);
-                    resolve();
-                }
-            }, 250)
+            ];
+            animateStringList(list);
+        }
+    }
+
+    const transitionGlobe = ()=>{
+        setGlobeStyle({
+
         });
     }
 
-    const startAnimation = () => {
-
-    }
 
     return (
         <div
@@ -140,6 +228,35 @@ export const HomeScreen:React.FC<Props> = ({scrollPosition})=>{
                 <div style={globeStyle}>
                     <SkillsGlobe />
                 </div>
+                <h4>
+                    {nameSentence}
+                </h4>
+                <h6>
+                    {nickNameSentence}
+                </h6>
+                <div 
+                    style={
+                        {
+                            display:"flex",
+                            flexDirection: "row"
+                        }
+                    }  
+                >
+                    <h5>{loveSentence}</h5>
+                    <ul
+                        style={
+                            {
+                                listStyle:"none",
+                                margin: "0px",
+                                padding: 0
+                            }
+                        }
+                    >
+                        {loveList.map((list, index) => <li key={list} style={listStyle[index]}><h5>{list}</h5></li>)}
+                        <li onTransitionEnd={onListTransitionEnd} style={listStyle[loveList.length]}><p>except printers...CAUSE UGH printers....</p></li>
+                    </ul>
+                </div>
+                <h4>{titleSentence}</h4>
             </div>
             <div style={middleStyle}>
                 <img
