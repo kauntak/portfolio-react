ed7271f1300db49aa72247e746c9ee2c6f2181b5
kauntak <73092120+kauntak@users.noreply.github.com>
2022-08-29 02:15:38 -0600

    Adding menu bar animation

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { ProjectIcon } from "../components/ProjectComponent";
 import { Selector } from "../components/SelectorComponent";
 import { projects } from "../utils/projects";
 import { SelectorType } from "../types";
 import { MinifiedContext } from "../context/MinifiedContext";
+import { ScrollContext } from "../context/Scroll";
 
 export const projectTypeList = [
     "All",
@@ -16,6 +17,38 @@ export const projectTypeList = [
 export const Portfolio:React.FC = ()=>{
     const [current, setCurrent] = useState<SelectorType>("All");
     const isMinified = useContext(MinifiedContext);
+    const selectorContainerRef = useRef<HTMLDivElement>(null);
+    const [headerHasBorder, setHeaderHasBorder] = useState<boolean>(false);
+    const animated = useRef<boolean>(false)
+    const scrollPosition = useContext(ScrollContext);
+
+    const animateSelector = () => {
+        let i = 1;
+        console.log("animation start");
+        
+        const interval = setInterval(()=> {
+            if(i === projectTypeList.length){
+                setHeaderHasBorder(false);
+                setCurrent(projectTypeList[0]);
+                clearInterval(interval);
+                return;
+            }
+            setHeaderHasBorder(i % 2 === 1);
+            setCurrent(projectTypeList[i]);
+        }, 750)
+    }
+
+    useEffect(()=> {
+        if(selectorContainerRef.current === null || animated.current) return;
+        console.log("passed first test");
+        
+        if(window.pageYOffset + window.innerHeight >= selectorContainerRef.current.offsetTop){
+            console.log("is in view:")
+            animated.current = true;
+            animateSelector();
+        }
+    }, [scrollPosition]);
+
     
 
     const onGridClick = (e:React.MouseEvent<HTMLDivElement>) => {
@@ -36,7 +69,9 @@ export const Portfolio:React.FC = ()=>{
                     marginLeft: "10vw"
                 }}
             >
-                <div>
+                <div
+                    ref={selectorContainerRef}
+                >
                     <Selector 
                         list={projectTypeList.map(item => item)} 
                         current={current} 
@@ -49,7 +84,9 @@ export const Portfolio:React.FC = ()=>{
                                     marginTop: 20,
                                     marginRight: "min(20px, 5vw)",
                                     marginLeft: "min(25px, 15vw)",
-                                    zIndex: 20
+                                    zIndex: 20,
+                                    boxShadow: headerHasBorder? "0 0 0 10px var(--accent)" : undefined,
+                                    transition: headerHasBorder?"box-shadow 0.75s ease-in-out" : undefined
                                 }
                                 :{
                                     marginTop:"20px",
