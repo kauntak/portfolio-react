04241b97c7680e926e554c4f28cac176f12abaab
kauntak <n.koshirae+github@gmail.com>
2022-08-28 22:10:07 -0600

    Update AccordionComponent.tsx

--- src/components/AccordionComponent.tsx
+++ src/components/AccordionComponent.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { ReactElement, useState } from "react";
 
 type Props = {
-
+    title:string,
+    content:ReactElement
 }
 
-export const Accordion:React.FC<Props> = ({}) => {
+export const Accordion:React.FC<Props> = ({title, content}) => {
+    const [isShown, setIsShown] = useState<boolean>(false);
+
     return (
         <>
+            <div>
+                
+            </div>
+            <div
+            >
+                {content}
+            </div>
         </>
     )
 }
