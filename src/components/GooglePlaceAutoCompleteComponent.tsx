73644c69686f828d6b4d91eae6facc2108acbc82
kauntak <73092120+kauntak@users.noreply.github.com>
2022-08-30 02:11:58 -0600

    Update GooglePlaceAutoCompleteComponent.tsx
    Added style props

--- src/components/GooglePlaceAutoCompleteComponent.tsx
+++ src/components/GooglePlaceAutoCompleteComponent.tsx
@@ -2,15 +2,16 @@ import React, { CSSProperties, Dispatch, SetStateAction, useEffect, useRef, useS
 import { handleMapsScriptLoad } from "../utils/googleAPI/googlePlaces";
 import { loadScript } from "../utils/helpers";
 
-const mapsURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`
+const mapsURL = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}&libraries=places`
 
 type Props = {
-    style:CSSProperties,
+    style?:CSSProperties,
+    name?:string,
     input: string,
     setInput: Dispatch<SetStateAction<string>>
 }
 
-export const LocationSearchInput:React.FC<Props> = ({style, input, setInput}) => {
+export const LocationSearchInput:React.FC<Props> = ({style, input, name, setInput}) => {
     const inputRef = useRef<HTMLInputElement>(null);
 
     const onInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
@@ -21,10 +22,12 @@ export const LocationSearchInput:React.FC<Props> = ({style, input, setInput}) =>
         loadScript(mapsURL, ()=> {
             handleMapsScriptLoad(setInput, inputRef);
         })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
         <input
+            name={name}
             value={input}
             onChange={onInputChange}
             placeholder="Address"
