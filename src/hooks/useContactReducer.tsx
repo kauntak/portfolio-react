34e28981ccad500d20d55eecb3aa43813353fe02
kauntak <73092120+kauntak@users.noreply.github.com>
2022-08-30 02:12:53 -0600

    Update useContactReducer.tsx
    moved types to type.d.ts
    removed commented out code
    added blur dispatch action

--- src/hooks/useContactReducer.tsx
+++ src/hooks/useContactReducer.tsx
@@ -1,46 +1,8 @@
-// name, setName] = useState<string>("");
-//     const [email, setEmail] = useState<string>("");
-//     const [phone, setPhone] = useState<string>("");
-//     const [address, setAddress] = useState<string>("");
-//     const [preferred, setPreferred] = useState<number>(0);
-//     const [robotIndex, setRobotIndex] = useState<number>(0);
-//     const [message, setMessage] = useState<string>
-
 import { Dispatch, Reducer, ReducerState, useReducer } from "react";
 import { CONTACT_TYPE } from "../pages/Contact";
-
-export type FieldType = "name" | "email" | "phone" | "fax" | "address" | "preferred" | "message";
-
-
-type InputChangeAction = {
-    type:"input",
-    payload:{
-        field:FieldType,
-        value:string
-    }
-}
-
-type PreferredChangeAction = {
-    type:"preferred",
-    payload: string
-}
-
-export type ContactActionType = InputChangeAction | PreferredChangeAction
+import { ContactActionType, ContactMatchType, ContactStateType, FieldType } from "../types";
 
 
-export type ContactStateType = {
-    [key in FieldType]:{
-        value:string,
-        required?:boolean,
-        isError?:boolean
-    }
-}
-
-type ContactTypeType = typeof CONTACT_TYPE[number];
-
-type ContactMatchType = {
-    [key in ContactTypeType]:FieldType
-}
 
 const CONTACT_MATCH:ContactMatchType = {
     "Carrier Pigeon":"address",
@@ -75,21 +37,53 @@ const handlePreferred = (state:ContactStateType, value:string):ContactStateType 
     }
     if(newField === prevField) return newState;
     newState[prevField].required = false;
-    newState[prevField].isError = false;
+    newState[prevField].isError = undefined;
     newState[newField].required = true;
     return newState;
 }
 
+const isValidPhoneNumber = (input:string):boolean => {
+    const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
+    return phoneRegex.test(input)
+}
+
+const isValidEmail = (input:string):boolean => {
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return emailRegex.test(input);
+}
+
+
+
+const handleBlur = (state:ContactStateType, field:FieldType):ContactStateType => {
+    const newState = {
+        ...state
+    }
+    if(state[field].value === "") return newState;
+    switch(field){
+        case "email":
+            if(!isValidEmail(state[field].value)){
+                state[field].isError = "invalid";
+            }
+            break;
+        case "fax":
+        case "phone":
+            if(!isValidPhoneNumber(state[field].value)){
+                state[field].isError = "invalid"
+            }
+        }
+    return newState;
+}
 
 const init = ():ContactStateType => {
     return {
-        name: {value:""},
-        email: {value:""},
+        name: {value:"", required:true},
+        email: {value:"", required:true},
         phone: {value:""},
         address: {value:""},
         fax: {value:""},
         preferred: {value:"0"},
-        message: {value:""}
+        subject: {value:"", required:true},
+        message: {value:"", required:true}
     }
 }
 
@@ -99,6 +93,8 @@ const reducer = (state:ContactStateType, action:ContactActionType):ContactStateT
             return handleInput(state, action.payload);
         case "preferred":
             return handlePreferred(state, action.payload);
+        case "blur":
+            return handleBlur(state, action.payload);
     }
 }
 
