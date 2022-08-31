1cdb5fe39d34b2795102612284ef1d5f8529ea4a
kauntak <n.koshirae+github@gmail.com>
2022-08-30 21:00:11 -0600

    Update useContactReducer.tsx
    -Minor adjustment to email check regex
    -Added dispatch action that check for errors on form submit
    -Adjustment to types and variable names

--- src/hooks/useContactReducer.tsx
+++ src/hooks/useContactReducer.tsx
@@ -1,18 +1,10 @@
 import { Dispatch, Reducer, ReducerState, useReducer } from "react";
-import { CONTACT_TYPE } from "../pages/Contact";
-import { ContactActionType, ContactMatchType, ContactStateType, FieldType } from "../types";
+import { CONTACT_FIELD_MAP, CONTACT_TYPE } from "../pages/Contact";
+import { ContactActionType, ContactStateType, FieldType } from "../types";
 
 
 
-const CONTACT_MATCH:ContactMatchType = {
-    "Carrier Pigeon":"address",
-    "Fax":"fax",
-    "Phone(Call)": "phone",
-    "Phone(Text)": "phone",
-    "Post(Letter)": "address",
-    "Telegram": "address",
-    "eMail": "email"
-} as const;
+
 
 const handleInput = (state:ContactStateType, {field, value}:{field:FieldType, value:string}):ContactStateType => {
     const newState = {
@@ -20,24 +12,25 @@ const handleInput = (state:ContactStateType, {field, value}:{field:FieldType, va
         [field]:{
             ...state[field],
             value,
-            isError: false
+            error: false
         }
     }
     return newState
 }
 const handlePreferred = (state:ContactStateType, value:string):ContactStateType => {
     const prevValue:number = parseInt(state.preferred.value);
-    const prevField = CONTACT_MATCH[CONTACT_TYPE[prevValue]]; 
-    const newField = CONTACT_MATCH[CONTACT_TYPE[parseInt(value)]];
+    const prevField:FieldType = CONTACT_FIELD_MAP[CONTACT_TYPE[prevValue]]; 
+    const newField:FieldType = CONTACT_FIELD_MAP[CONTACT_TYPE[parseInt(value)]];
     const newState:ContactStateType = {
         ...state,
         preferred:{
-            value:value.toString()
+            value,
+            required:true
         }
     }
     if(newField === prevField) return newState;
     newState[prevField].required = false;
-    newState[prevField].isError = undefined;
+    newState[prevField].error = undefined;
     newState[newField].required = true;
     return newState;
 }
@@ -48,7 +41,7 @@ const isValidPhoneNumber = (input:string):boolean => {
 }
 
 const isValidEmail = (input:string):boolean => {
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
     return emailRegex.test(input);
 }
 
@@ -62,16 +55,27 @@ const handleBlur = (state:ContactStateType, field:FieldType):ContactStateType =>
     switch(field){
         case "email":
             if(!isValidEmail(state[field].value)){
-                state[field].isError = "invalid";
+                state[field].error = "invalid";
             }
             break;
         case "fax":
         case "phone":
             if(!isValidPhoneNumber(state[field].value)){
-                state[field].isError = "invalid"
+                state[field].error = "invalid"
             }
         }
     return newState;
+}
+
+const handleRequireCheck = (state:ContactStateType):ContactStateType => {
+    const newState = {...state};
+    let field:FieldType;
+    for(field in state){
+        if(state[field].required && state[field].value===""){
+            state[field].error = "required"
+        }
+    }
+    return newState
 }
 
 const init = ():ContactStateType => {
@@ -81,7 +85,7 @@ const init = ():ContactStateType => {
         phone: {value:""},
         address: {value:""},
         fax: {value:""},
-        preferred: {value:"0"},
+        preferred: {value:"0", required:true},
         subject: {value:"", required:true},
         message: {value:"", required:true}
     }
@@ -95,6 +99,8 @@ const reducer = (state:ContactStateType, action:ContactActionType):ContactStateT
             return handlePreferred(state, action.payload);
         case "blur":
             return handleBlur(state, action.payload);
+        case "requiredCheck":
+            return handleRequireCheck(state);
     }
 }
 
