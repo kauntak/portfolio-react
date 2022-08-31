15a65b32e6626b4781267b83ddd1af20b7935b99
kauntak <n.koshirae+github@gmail.com>
2022-08-30 21:04:37 -0600

    Update helpers.tsx
    Added send mail function(uses postmail)

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,19 +1,56 @@
+import { ContactPreferenceType, SendStateType } from "../types";
+
 export const loadScript = (url: string, callback: Function) => {
+    const id = "googlePlacesAPIScript"
+    if(document.getElementById(id) !== null) {
+        callback();
+        return;
+    }
     const script = document.createElement("script");
+    script.id = id;
     script.src = url;
     script.type = "text/javascript";
+    script.onload = () => callback();
+    
+    document.head.appendChild(script);
+}
 
-    if ((script as any).readyState) {
-        (script as any).onreadystatechange = function () {
-            if ((script as any).readyState === "loaded" || (script as any).readyState === "complete") {
-                (script as any).onreadystatechange = null;
-                callback();
+const postmailUrl = "https://postmail.invotes.com/send";
+
+export const sendMail = (name:string, preferredMethod:ContactPreferenceType, preferredValue:string, subject:string, message:string):Promise<SendStateType> => {
+    return new Promise((resolve, reject) => {
+        const data = {
+            access_token: process.env.REACT_APP_POSTMAIL_API_KEY || "",
+            subject: `Contact from: ${name} - ${subject}`,
+            text:`${preferredMethod} - ${preferredValue}
+            
+            ${message}`
+        }
+        const params = toParams(data);
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = () => {
+            if(request.readyState === 4 && request.status === 200){
+                resolve("success");
+            } else {
+                reject("failure");
             }
         };
-    } else {
-        script.onload = () => callback();
+        request.open("POST", postmailUrl, true);
+        request.setRequestHeader(
+            "Content-type",
+            "application/x-www-form-urlencoded"
+        );
+
+        request.send(params);
+    })
+}
+
+const toParams = (data:{[key:string]:string}) => {
+    var formData = [];
+    for(let key in data){
+        formData.push(
+            `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
     }
-
-
-    document.head.appendChild(script);
+    return formData.join("&");
 }
