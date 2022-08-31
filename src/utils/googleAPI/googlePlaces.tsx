ecb69f2271b9219b62710813abf51bc63a2260de
kauntak <n.koshirae+github@gmail.com>
2022-08-30 21:03:53 -0600

    Update googlePlaces.tsx
    -Set a check to ensure google API scrips were loaded
    -Changed setStateAction to Reducer Dispatch Action

--- src/utils/googleAPI/googlePlaces.tsx
+++ src/utils/googleAPI/googlePlaces.tsx
@@ -1,20 +1,28 @@
-import { Dispatch, MutableRefObject, SetStateAction } from "react";
+import { Dispatch, MutableRefObject } from "react";
+import { ContactActionType } from "../../types";
 
 let autoComplete:any;
 
 
-export const handleMapsScriptLoad = (setter:Dispatch<SetStateAction<string>>, inputRef:MutableRefObject<HTMLInputElement|null>) => {
-    autoComplete = new (window as any).google.maps.places.Autocomplete(inputRef.current);
-    autoComplete.setFields(["address_component", "formatted_address"]);
-    autoComplete.addListener("place_changed", () => {
-        handlePlaceSelect(setter);
-    })
+export const handleMapsScriptLoad = (dispatch: Dispatch<ContactActionType>,inputRef:MutableRefObject<HTMLInputElement|null>) => {
+    const interval = setInterval(()=> {
+        if(inputRef.current === null || (window as any).google?.maps?.places?.Autocomplete === undefined) return;
+        clearInterval(interval);
+        setAutoComplete();
+    }, 50);
+    const setAutoComplete = () => {
+        autoComplete = new (window as any).google.maps.places.Autocomplete(inputRef.current);
+        autoComplete.setFields(["address_component", "formatted_address"]);
+        autoComplete.addListener("place_changed", () => {
+            handlePlaceSelect(dispatch);
+        })
+    }
 }
 
-const handlePlaceSelect = (setter:Dispatch<SetStateAction<string>>) => {
+const handlePlaceSelect = (dispatch: Dispatch<ContactActionType>) => {
     const addressObject = autoComplete.getPlace();
     console.log(addressObject);
     
     const newAddress = addressObject.formatted_address;
-    setter(newAddress);
+    dispatch({type:"input", payload:{field:"address", value: newAddress}});
 }
