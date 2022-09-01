ad52749cc92dc9cc52abacfc9f28c1a005d143e8
kauntak <n.koshirae+github@gmail.com>
2022-09-01 11:37:44 -0600

    Update SkillsComponent.tsx
    updated tag canvas to change colors depending on theme

--- src/components/SkillsComponent.tsx
+++ src/components/SkillsComponent.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { ThemeContext } from "../context/ThemeContext";
+import { ThemeType } from "../types";
 import { loadScript } from "../utils/helpers";
 import { skills } from "../utils/skills";
 import { startTagCanvas } from "../utils/tagCanvas/tagCanvas";
@@ -9,12 +11,32 @@ type Props = {
 
 const TAG_CANVAS_URL = "/scripts/tagCanvas.js";
 
+
+const THEME_COLOR_MAP:{[key in ThemeType]:string} = {
+    light:"#0d5954", 
+    dark:"#17d4c7",
+    halloween:"#FF5F1F",
+    darkRoom:"#fffafa", 
+    orangeAndTeal:"#FF5F1F",
+    purpleMurple:"#b09bc7", 
+    vaporWave:"#ee00ff", 
+    valentines:"#f7b2c7",
+    imBlueDabaDee:"#0000ff", 
+    ohGodMyEyes:"#00ffee", 
+    "6ShadesOfGray":"#000000"
+}
+
 export const SkillsGlobe:React.FC<Props> = ({isVisible})=>{
     const [isError, setIsError] = useState<boolean>(false);
+    const theme = useContext(ThemeContext)
     
     useEffect(()=>{
-        loadScript(TAG_CANVAS_URL, "tagCanvasAPI", () => startTagCanvas(setIsError));
+        loadScript(TAG_CANVAS_URL, "tagCanvasAPI", () => {});
     }, []);
+
+    useEffect(()=> {
+        startTagCanvas(setIsError, THEME_COLOR_MAP[theme]);
+    }, [theme])
 
     return (
         <div
