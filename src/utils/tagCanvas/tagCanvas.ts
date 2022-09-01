a64bc9dd6fda7bdc55f4a32f3f259ab70c25d892
kauntak <n.koshirae+github@gmail.com>
2022-08-31 18:22:38 -0600

    Update tagCanvas.ts

--- src/utils/tagCanvas/tagCanvas.ts
+++ src/utils/tagCanvas/tagCanvas.ts
@@ -1,19 +1,27 @@
 import { Dispatch, SetStateAction } from "react";
 
-export const StartTagCanvas = (setIsError:Dispatch<SetStateAction<boolean>>) => {
-    const TagCanvas = (window as any).TagCanvas;
-    TagCanvas.textColour = "#DEB221";
-    TagCanvas.outlineColour = "var(--accent)";
-    TagCanvas.outlineMethod = "colour";
-    TagCanvas.shuffleTags = true;
-    TagCanvas.initial = [-0.1, 0.07];
-    TagCanvas.minSpeed = 0.02;
-    TagCanvas.maxSpeed = 0.09;
-    TagCanvas.wheelZoom = false;
-    try{
-        TagCanvas.Start("tagCanvas", "tagList");
-    } catch (e) {
-        console.log("Canvas Error", e);
-        setIsError(true);
-    }
+export const startTagCanvas = (setIsError:Dispatch<SetStateAction<boolean>>) => {
+    const interval = setInterval(()=> {
+        if((window as any).TagCanvas) {
+            clearInterval(interval);
+            initializeTagCanvas();
+        }
+    })
+    const initializeTagCanvas = () => {
+        const TagCanvas = (window as any).TagCanvas;
+        TagCanvas.textColour = "#DEB221";
+        TagCanvas.outlineColour = "var(--accent)";
+        TagCanvas.outlineMethod = "colour";
+        TagCanvas.shuffleTags = true;
+        TagCanvas.initial = [-0.1, 0.07];
+        TagCanvas.minSpeed = 0.02;
+        TagCanvas.maxSpeed = 0.09;
+        TagCanvas.wheelZoom = false;
+        try{
+            TagCanvas.Start("tagCanvas", "tagList");
+        } catch (e) {
+            console.log("Canvas Error", e);
+            setIsError(true);
+        }
+    }   
 }
