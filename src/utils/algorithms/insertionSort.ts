0d18417ad7db4a3f48d67d5d884309df6134f1a5
kauntak <n.koshirae+github@gmail.com>
2022-09-01 15:09:01 -0600

    Update insertionSort.ts
    Coded selection sort instead of insertion sort. Now is insertion sort.

--- src/utils/algorithms/insertionSort.ts
+++ src/utils/algorithms/insertionSort.ts
@@ -5,28 +5,34 @@ export const insertion = (oldBlocks:Arr<number>):SequenceType[] => {
     const sequence:SequenceType[] = [];
     const blocks = oldBlocks.copy();
     for(let i = 0; i < blocks.length; i++){
-        for(let j = i + 1; j < blocks.length; j++){
+        const completeSequence:SequenceType = {
+            type:"complete",
+            index: i
+        }
+        sequence.push(completeSequence);
+        for(let j = 0; j < i; j++){
             const compareSequence:SequenceType = {
                 type:"compare",
                 indexA: j,
                 indexB: i
             }
             sequence.push(compareSequence);
-            if(blocks.isFirstLarger(i, j)){
-                const swapSequence:SequenceType = {
-                    type:"swap",
-                    indexA: i,
-                    indexB: j,
-                    blocks: blocks.swap(i, j)
+            if(blocks.isFirstLarger(j, i)){
+                const insertSequence:SequenceType = {
+                    type:"insert",
+                    indexFrom: i,
+                    indexTo: j,
+                    blocks: blocks.insert(i, j)
                 }
-                sequence.push(swapSequence);
+                sequence.push(insertSequence);
+                const completeSequence:SequenceType = {
+                    type:"complete",
+                    index: i
+                }
+                sequence.push(completeSequence);
+                break;
             }
         }
-        const completeSequence:SequenceType = {
-            type:"complete",
-            index: i
-        }
-        sequence.push(completeSequence);
     }
     return sequence;
 }
